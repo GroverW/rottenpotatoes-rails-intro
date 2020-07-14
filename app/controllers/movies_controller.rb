37e15c175610535b47be416ef78d5be5ee8be157
GroverW <cheesemaker@gmail.com>
2020-07-14 16:03:40 -0700

    added skeleton of ratings to view, controller and mdoel

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,6 +17,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all.order(params[:sort])
+    @all_ratings = Movie.all_ratings
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    []
+  end
+  
+  def self.with_ratings
+
+  end
 end
