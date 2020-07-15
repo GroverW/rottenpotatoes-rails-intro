0e676db31bca55eefedf83063838edb5289ead84
GroverW <cheesemaker@gmail.com>
2020-07-14 17:19:36 -0700

    added with_ratings scope to movies model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,9 @@
 class Movie < ActiveRecord::Base
+  scope :with_ratings, ->ratings { 
+    where('upper(rating) IN (?)', ratings.keys.map{ |r| r.upcase }) unless ratings.blank? 
+  }
+  
   def self.all_ratings
     Movie.uniq.order(:rating).pluck(:rating)
-  end
-  
-  def self.with_ratings
-
   end
 end
